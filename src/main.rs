5a23e660ac58b4ae3e3314e9f9ac848022e54770
Christopher Tee <christopherfytee@gmail.com>
2025-05-13 21:50:03 -0400

    🎉 Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use dioxus::{logger::tracing, prelude::*};
+
+static CSS: Asset = asset!("assets/main.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Stylesheet { href: CSS }
+        div { id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+        div { id: "dogview",
+            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        }
+        div { id: "buttons",
+            button { id: "skip", "skip" }
+            button { id: "save", "save!" }
+        }
+    }
+}
+
+#[component]
+fn DogApp(breed: String) -> Element {
+    tracing::info!("Rendered with breed: {breed}");
+    todo!()
+}
